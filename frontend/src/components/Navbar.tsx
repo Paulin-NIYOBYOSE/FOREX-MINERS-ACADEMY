60f08a12fe9935592bcfebe1723719153d7fd9be
Paulin-NIYOBYOSE <niyobyosepaulin20@gmail.com>
2024-08-29 19:42:24 +0200

    Added icons

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,7 +1,31 @@
+import { FiLogIn } from "react-icons/fi";
+import { MdDarkMode } from "react-icons/md";
 const Navbar = () => {
   return (
     <>
-      <div className=""></div>
+      <div className="">
+        <div>
+          <div>Logo</div>
+        </div>
+        <div>
+          <ul>
+            <li>Home</li>
+            <li>Mentorship</li>
+            <li>Signals</li>
+            <li>Minor AI</li>
+            <li>About us</li>
+          </ul>
+        </div>
+        <div>
+          <button>Log in </button>
+          <button>
+            <FiLogIn />
+          </button>
+          <button>
+            <MdDarkMode />
+          </button>
+        </div>
+      </div>
     </>
   );
 };
